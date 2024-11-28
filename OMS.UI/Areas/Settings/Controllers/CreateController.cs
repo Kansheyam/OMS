774f06ac1e7e752ff5622e233342484dbc0b0944
Admin <Admin@DESKTOP-EQDIFDS>
2024-11-28 10:51:33 +0530

    Add project files.

---
+++ OMS.UI/Areas/Settings/Controllers/CreateController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OMS.UI.Areas.Settings.Controllers
+{
+    public class CreateController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Page Title
+            ViewData["pTitle"] = "Create Controll";
+
+            // Breadcrumb
+            ViewData["bGParent"] = "Settings";
+            ViewData["bParent"] = "Company";
+            ViewData["bChild"] = "Dashboard";
+
+            return View();
+        } public IActionResult Consumer()
+        {
+            // Page Title
+            ViewData["pTitle"] = "Create Controll";
+
+            // Breadcrumb
+            ViewData["bGParent"] = "Settings";
+            ViewData["bParent"] = "Consumer Settings";
+            ViewData["bChild"] = "Consumer Creation";
+
+            return View();
+        } public IActionResult Planning()
+        {
+            // Page Title
+            ViewData["pTitle"] = "Create Controll";
+
+            // Breadcrumb
+            ViewData["bGParent"] = "Settings";
+            ViewData["bParent"] = "Planning Settings";
+            ViewData["bChild"] = "Planning Creation";
+
+            return View();
+        }
+    }
+}
